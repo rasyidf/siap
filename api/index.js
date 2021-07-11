62c2b198ee256101e8a5634f40befa9c8aaf81e3
rasyidf <ufarasfa@gmail.com>
2021-07-11 12:09:54 +0700

    Add API & Data

---
+++ api/index.js
@@ -0,0 +1,37 @@
+const { join } = require('path') 
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({
+  extended: true,
+  limit: '50mb'
+}))
+app.use(cookieParser())
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE')
+  res.header('Access-Control-Allow-Headers', 'origin, content-type, Authorization')
+  res.header('Content-Type', 'application/json')
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+
+// Data route
+const dataFolder = join(__dirname, '..', 'data')
+app.use('/data', express.static(dataFolder))
+
+// API routes
+const apiRoutes = express.Router()
+apiRoutes.use('/user', require('./controller/user-controller'))
+apiRoutes.use('/face', require('./controller/face-controller'))
+app.use('/api', apiRoutes)
+
+module.exports = {
+  path: '/',
+  handler: app
+}
